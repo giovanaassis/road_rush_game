274a3f9599e997b2c87f91299b5abef7b8bb1cac
giovanaassis <giovana_2011_11@hotmail.com>
2025-08-28 20:35:45 -0300

    feat: update level.py

--- code/Level.py
+++ code/Level.py
@@ -1,5 +1,10 @@
-import pygame
+import sys
 
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_HEIGHT, C_WHITE
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -10,12 +15,29 @@ class Level:
         self.name = name
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity("RoadBg"))
+        self.timeout = 20000  # 20 SECONDS
 
     def run(self):
         clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
                 self.window.blit(ent.surf, ent.rect)
                 ent.move()
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.level_text(20, f"Timeout: {self.timeout / 1000 :.1f}s", C_WHITE, (70, 30))
+            self.level_text(20, f'entidades: {len(self.entity_list)}', C_WHITE, (60, WIN_HEIGHT - 20))
+
             pygame.display.flip()
-            clock.tick(60)
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont("impact", text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(text_surf, text_rect)
