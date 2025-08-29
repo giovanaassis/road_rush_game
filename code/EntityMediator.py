c9d72466716d7ef803ecf7e26680ec0f97f98459
giovanaassis <giovana_2011_11@hotmail.com>
2025-08-29 00:11:55 -0300

    feat: finished the game logic

--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,5 +1,6 @@
 from code.Const import WIN_HEIGHT, WIN_WIDTH
 from code.Entity import Entity
+from code.Player import Player
 from code.Traffic import Traffic
 
 
@@ -12,10 +13,27 @@ class EntityMediator:
                 ent.health = 0
 
     @staticmethod
+    def __verify_collision_entity(ent1, ent2):
+        valid_interaction = False
+        if isinstance(ent1, Player) and isinstance(ent2, Traffic):
+            valid_interaction = True
+        elif isinstance(ent1, Traffic) and isinstance(ent2, Player):
+            valid_interaction = True
+
+        if valid_interaction:
+            if ent1.rect.colliderect(ent2.rect):
+                ent1.health = 0
+                ent2.health = 0
+
+
+    @staticmethod
     def verify_collision(entity_list: list[Entity]):
         for i in range(len(entity_list)):
-            ent = entity_list[i]
-            EntityMediator.__verify_collision_window(ent)
+            ent1 = entity_list[i]
+            EntityMediator.__verify_collision_window(ent1)
+            for j in range(i + 1, len(entity_list)):
+                ent2 = entity_list[j]
+                EntityMediator.__verify_collision_entity(ent1, ent2)
 
     @staticmethod
     def verify_health(entity_list: list[Entity]):
--- code/Level.py
+++ code/Level.py
@@ -4,10 +4,11 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_HEIGHT, C_WHITE, EVENT_TRAFFIC, SPAWN_TIME
+from code.Const import WIN_HEIGHT, C_WHITE, EVENT_TRAFFIC, SPAWN_TIME, EVENT_TIMEOUT, TIMEOUT_STEP, WIN_WIDTH
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 from code.EntityMediator import EntityMediator
+from code.Player import Player
 
 
 class Level:
@@ -19,6 +20,7 @@ class Level:
         self.entity_list.append(EntityFactory.get_entity("Car"))
         self.timeout = 20000  # 20 SECONDS
         pygame.time.set_timer(EVENT_TRAFFIC, SPAWN_TIME)
+        pygame.time.set_timer(EVENT_TIMEOUT, TIMEOUT_STEP)
 
     def run(self):
         clock = pygame.time.Clock()
@@ -35,7 +37,19 @@ class Level:
                     sys.exit()
                 if event.type == EVENT_TRAFFIC:
                     self.entity_list.append(EntityFactory.get_entity("Traffic"))
+                if event.type == EVENT_TIMEOUT:
+                    self.timeout -= TIMEOUT_STEP
+                    if self.timeout == 0:
+                        self.show_win_screen()
+                        return True
 
+            found_player = False
+            for ent in self.entity_list:
+                if isinstance(ent, Player):
+                    found_player = True
+
+            if not found_player:
+                return False
 
             self.level_text(20, f"Timeout: {self.timeout / 1000 :.1f}s", C_WHITE, (70, 30))
             self.level_text(20, f'entidades: {len(self.entity_list)}', C_WHITE, (60, WIN_HEIGHT - 20))
@@ -43,6 +57,22 @@ class Level:
 
             EntityMediator.verify_collision(self.entity_list)
             EntityMediator.verify_health(self.entity_list)
+
+
+    def show_win_screen(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        return False
+
+            self.level_text(40, "YOU WIN!", C_WHITE, (WIN_WIDTH // 2, WIN_HEIGHT // 2))
+            self.level_text(40, "PRESS ENTER", C_WHITE, (WIN_WIDTH // 2, WIN_HEIGHT // 2 + 80))
+            pygame.display.flip()
+
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont("impact", text_size)
